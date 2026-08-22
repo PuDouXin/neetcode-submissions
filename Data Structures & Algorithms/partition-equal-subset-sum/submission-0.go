ec81b76b79418ce74aed1708d337664c847d3430
PuDouXin <wangxin.ivy@gmail.com>
2026-08-22 16:17:27 -0400

    Add: partition-equal-subset-sum - submission-0

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-0.go
@@ -0,0 +1,21 @@
+func canPartition(nums []int) bool {
+	total := 0
+	for _, num := range nums{
+		total += num
+	}
+	if total % 2 != 0{
+		return false
+	}
+	target := total/2
+    dp := make([]bool, target+1)
+	dp[0] = true
+	for _, num := range nums{
+		for i := target; i>=num;i--{
+			dp[i] = dp[i] || dp[i-num]
+			if dp[target]{
+				return true
+			}
+		}
+	}
+	return dp[target]
+}
