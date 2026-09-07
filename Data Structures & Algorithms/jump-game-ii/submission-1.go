5b323e302cb38296c94afb0265b8fcfef7247f2b
PuDouXin <wangxin.ivy@gmail.com>
2026-09-07 15:25:10 -0400

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.go
@@ -0,0 +1,15 @@
+func jump(nums []int) int {
+    res,l,r := 0,0,0
+
+	for r < len(nums)-1{
+		farthest :=0
+		for i:=l;i<=r;i++{
+			farthest = max(farthest, nums[i]+i)
+		}
+		l = r+1
+		r = farthest
+		res++
+	}
+	return res
+
+}
